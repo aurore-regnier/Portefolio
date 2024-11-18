78f2f078b8b3d534061dbc5881e0cd55ae1ebcf6
aurore-regnier <auroregnier21@gmail.com>
2024-11-18 14:47:43 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuGrid = document.querySelector ('.menuGrid');
+const menuIcon = document.querySelector ('.menuIcon');
+
+menuIcon.addEventListener('click', () =>
+{
+menuGrid.classList.toggle('active')
+})
+
